f40fd9d6b00fb9a7dbd31593b90828c9b00ea73a
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-01-25 21:21:25 +0100

    Rotate SSH keys

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   lucasf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIADrfGE05Zd40h0IoIsXaewcD1AWNPd12AhJG6h9c8N0 lucasf";
-  nixos-init-test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINkIAy/GCAVgHvEJTJY/4gIETTMxX6hLBEi66gfo7a2u root@nixos-init-test";
-  axiom-vm-wireguard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEaIgWALhP+nMo1EztwRjWYkpzQ1rNIpjVrbmN8uwBvL root@axiom-vm-wireguard";
+  nixos-init-test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWrKft1r1LohZt20beWxPhjlkhVTC37TMBZ3ktTG94P root@nixos-init-test";
+  axiom-vm-wireguard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxKUS405qW1bI+JxT+xcHQayNqaCG02uffozmX56+vJ root@axiom-vm-wireguard";
 in
 {
   "secret.txt.age".publicKeys = [ lucasf ];
--- nix/utility/general.nix
+++ nix/utility/general.nix
@@ -13,6 +13,7 @@
 
   environment.systemPackages = [
     pkgs.git
+    pkgs.ragenix
   ];
 
   # Set your time zone.
