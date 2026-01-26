eee497f0219b42763878a9758ec753a20ca06f7d
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-01-26 23:21:41 +0100

    Add Kubernetes related utility tools to k3s-master

--- nix/hosts/axiom-vm-k3s-master/configuration.nix
+++ nix/hosts/axiom-vm-k3s-master/configuration.nix
@@ -3,4 +3,11 @@
 {
   networking.hostName = "axiom-vm-k3s-master";
   host.ipv4 = "10.67.1.103";
+
+  environment.systemPackages = [
+    pkgs.kubectl
+    pkgs.cilium-cli
+    pkgs.velero
+    pkgs.vim
+  ];
 }
