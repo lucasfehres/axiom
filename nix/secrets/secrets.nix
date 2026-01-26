04f8722a4649ea32e89f965eb7325f1ad648d0c9
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-01-26 23:11:06 +0100

    Cycle axiom-vm-k3s-master host keys

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   lucasf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIADrfGE05Zd40h0IoIsXaewcD1AWNPd12AhJG6h9c8N0 lucasf";
   nixos-init-test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWrKft1r1LohZt20beWxPhjlkhVTC37TMBZ3ktTG94P root@nixos-init-test";
   axiom-vm-wireguard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxKUS405qW1bI+JxT+xcHQayNqaCG02uffozmX56+vJ root@axiom-vm-wireguard";
-  axiom-vm-k3s-master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/xDiGHVW4YnDzb93KHLkyVg89S30eAE8wIeo+p5rXm root@axiom-vm-k3s-master";
+  axiom-vm-k3s-master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN//e1rKxMPXunyccEaIBLTVA2FwdCwwE2lxsy55oLcN root@axiom-vm-k3s-master";
 in
 {
   "secret.txt.age".publicKeys = [ lucasf ];
