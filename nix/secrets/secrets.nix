920a6ab6d6def346465c5a193eb85f675be64619
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-01-26 01:56:09 +0100

    Add K3s master SSH key

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -2,9 +2,10 @@ let
   lucasf = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIADrfGE05Zd40h0IoIsXaewcD1AWNPd12AhJG6h9c8N0 lucasf";
   nixos-init-test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWrKft1r1LohZt20beWxPhjlkhVTC37TMBZ3ktTG94P root@nixos-init-test";
   axiom-vm-wireguard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxKUS405qW1bI+JxT+xcHQayNqaCG02uffozmX56+vJ root@axiom-vm-wireguard";
+  axiom-vm-k3s-master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/xDiGHVW4YnDzb93KHLkyVg89S30eAE8wIeo+p5rXm root@axiom-vm-k3s-master";
 in
 {
   "secret.txt.age".publicKeys = [ lucasf ];
   "axiom-primary-wireguard-priv.key.age".publicKeys = [ lucasf axiom-vm-wireguard ];
-  "k3s-token.txt.age".publicKeys = [ lucasf ];
+  "k3s-token.txt.age".publicKeys = [ lucasf axiom-vm-k3s-master ];
 }
