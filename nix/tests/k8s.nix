b9b923e241eab1529b59882f3787d3600562f063
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-01-28 01:49:02 +0100

    Run Hubble in integration tests

--- nix/tests/k8s.nix
+++ nix/tests/k8s.nix
@@ -97,8 +97,21 @@ pkgs.testers.runNixOSTest {
       print(axiom_vm_utility.succeed("cilium status --wait"))
       print(axiom_vm_utility.succeed("kubectl get nodes"))
 
+      print("cloning axiom repository")
+      print(axiom_vm_utility.succeed("git clone https://github.com/lucasfehres/axiom.git /tmp/axiom"))
+
+      print("applying cilium configuration")
+      print(axiom_vm_utility.succeed("kubectl apply -f /tmp/axiom/nix/services/rke2/manifests/rke2-cilium.yaml"))
+
       time.sleep(10)
-      print("attempting cilium connectivity")
+      print("waiting for applied cilium configuration to be ready")
+      print(axiom_vm_utility.succeed("cilium status --wait"))
+
+      print("exposing hubble locally on the utility vm")
+      print(axiom_vm_utility.succeed("systemd-run --unit=hubble-port-forward --service-type=simple cilium --kubeconfig /root/.kube/config hubble port-forward"))
+
+      time.sleep(10)
+      print("running e2e cilium tests")
       print(axiom_vm_utility.succeed("cilium connectivity test"))
     '';
 }
