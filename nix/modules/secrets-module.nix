9f7aee322235473773f3fce9debc3bda768aed3a
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-02-09 22:48:09 +0100

    Make agenix secrets conditional

--- nix/modules/secrets-module.nix
+++ nix/modules/secrets-module.nix
@@ -1,6 +1,11 @@
 { lib, config, ... }:
 let
   cfg = config.axiom.secrets;
+  hostname = config.networking.hostName;
+
+  # prevents agenix errors when rebuilding
+  hasWireguard = builtins.elem hostname [ "axiom-vm-wireguard" ];
+  hasK3s = builtins.elem hostname [ "axiom-vm-k3s-master" ];
 in
 {
   options.axiom.secrets.enable = lib.mkOption {
@@ -10,13 +15,19 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    age.secrets.axiom-primary-wireguard-priv = {
-      file = ../secrets/axiom-primary-wireguard-priv.key.age;
-      mode = "640";
-      owner = "systemd-network";
-      group = "systemd-network";
-    };
+    age.secrets = lib.mkMerge [
+      (lib.mkIf hasWireguard {
+        axiom-primary-wireguard-priv = {
+          file = ../secrets/axiom-primary-wireguard-priv.key.age;
+          mode = "640";
+          owner = "systemd-network";
+          group = "systemd-network";
+        };
+      })
 
-    age.secrets.k3s-token.file = ../secrets/k3s-token.txt.age;
+      (lib.mkIf hasK3s {
+        k3s-token.file = ../secrets/k3s-token.txt.age;
+      })
+    ];
   };
 }
