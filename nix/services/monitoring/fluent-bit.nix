af3335fa04291f1def2b57f2f3203c71ca6192e8
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-02-22 22:15:04 +0100

    Deploy Fluent Bit to ship logs from Nix VMs to Loki

---
+++ nix/services/monitoring/fluent-bit.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.axiom.monitoring.kepler;
+in
+{
+  options.axiom.monitoring.fluent-bit.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Enable Fluent Bit log shipping";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.fluent-bit = {
+      enable = true;
+      settings = {
+        pipeline = {
+          inputs = [
+            {
+              name = "systemd";
+              # everything except debug
+              systemd_filter = "PRIORITY<=6";
+              read_from_tail = true;
+            }
+          ];
+          outputs = [
+            {
+              name = "loki";
+              match = "*";
+              labels = "job=nix-fluentbit, host=${networking.hostName}, unit=$_SYSTEMD_UNIT, priority=$PRIORITY";
+
+              host = "loki-write.internal.axiom.lucasfehres.nl";
+              port = 443;
+              tls = "on";
+            }
+          ];
+        };
+        service = {
+          grace = 30;
+        };
+      };
+    };
+  };
+}
--- nix/services/monitoring/monitoring.nix
+++ nix/services/monitoring/monitoring.nix
@@ -8,5 +8,6 @@
 {
   imports = [
     ./kepler.nix
+    ./fluent-bit.nix
   ];
 }
