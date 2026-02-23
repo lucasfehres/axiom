5d3fa8e5b7650f3a9b5cfdbc5b5a7602b679e41f
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-02-23 22:06:06 +0100

    Enable hardware drivers for laptop-test

--- nix/hosts/laptop-test/configuration.nix
+++ nix/hosts/laptop-test/configuration.nix
@@ -9,6 +9,11 @@
   # required for ZFS
   networking.hostId = "dead2bad";
 
+  # drivers
+  hardware.ipu6.enable = true;
+  hardware.ipu6.platform = "ipu6"; # not sure about this one
+  hardware.enableRedistributableFirmware = true;
+
   # this is an old NixOS install that was moved to this flake
   system.stateVersion = lib.mkForce "24.05";
 }
