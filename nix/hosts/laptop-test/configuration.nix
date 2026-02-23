327a763207002cc33adcbb7c6a2d0a3ba1834cd1
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-02-23 16:30:45 +0100

    Configure laptop filesystems

--- nix/hosts/laptop-test/configuration.nix
+++ nix/hosts/laptop-test/configuration.nix
@@ -4,4 +4,7 @@
   networking.hostName = "laptop-test";
   axiom.host.portable = true;
   axiom.host.gui = true;
+
+  # this is an old NixOS install that was moved to this flake
+  system.stateVersion = lib.mkForce "24.05";
 }
