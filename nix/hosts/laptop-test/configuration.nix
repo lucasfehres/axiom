b00e6d10c94d4ee53b0e55dd196829676b5c0957
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-02-23 16:35:05 +0100

    Add networking.hostId

--- nix/hosts/laptop-test/configuration.nix
+++ nix/hosts/laptop-test/configuration.nix
@@ -5,6 +5,9 @@
   axiom.host.portable = true;
   axiom.host.gui = true;
 
+  # required for ZFS
+  networking.hostId = "dead2bad";
+
   # this is an old NixOS install that was moved to this flake
   system.stateVersion = lib.mkForce "24.05";
 }
