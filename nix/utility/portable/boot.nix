699a377ff6a277976f1b9a14d2e699a3144fed7f
lucasfehres <180476097+lucasfehres@users.noreply.github.com>
2026-02-23 16:32:55 +0100

    Fix Nix configuration

--- nix/utility/portable/boot.nix
+++ nix/utility/portable/boot.nix
@@ -11,16 +11,16 @@ in
   config = lib.mkIf hostCfg.portable {
     boot.loader.systemd-boot.enable = true;
     boot.loader.systemd-boot.memtest86.enable = true;
-  };
 
-  fileSystems."/"     = { device = "zpool/root"; fsType = "zfs"; };
-  fileSystems."/nix"  = { device = "zpool/nix";  fsType = "zfs"; };
-  fileSystems."/var"  = { device = "zpool/var";  fsType = "zfs"; };
-  fileSystems."/home" = { device = "zpool/home"; fsType = "zfs"; };
+    fileSystems."/"     = { device = "zpool/root"; fsType = "zfs"; };
+    fileSystems."/nix"  = { device = "zpool/nix";  fsType = "zfs"; };
+    fileSystems."/var"  = { device = "zpool/var";  fsType = "zfs"; };
+    fileSystems."/home" = { device = "zpool/home"; fsType = "zfs"; };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/AB64-415C";
-    fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022"];
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/AB64-415C";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022"];
+    };
   };
 }
